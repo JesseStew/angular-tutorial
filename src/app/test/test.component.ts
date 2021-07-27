a184cc636786916fbcfc2a9197205a7ebc4d0467
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-07-27 15:14:25 -0500

    Property Binding Tutorial

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   public name = "Jesse"
+  public myId = "testId"
   constructor() { }
 
   ngOnInit(): void {
