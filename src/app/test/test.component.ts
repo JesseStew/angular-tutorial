579d278e1ff6720635a04a7d5e36a94ec2f42502
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-07-27 14:47:57 -0500

    Interpolation tutorial

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public name = "Jesse"
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser(){
+    return "Hello " + this.name
   }
 
 }
