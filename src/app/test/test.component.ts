eba1988a1e6e30e35982db816ab736e8b12c3d5d
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-07-28 09:58:40 -0500

    ngSwitch Directive Tutorial

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,37 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  public name = "Jesse"
-  public successClass = "text-success"
-  public hasError = true
-  public isSpecial = true
-  public highlightColor = "orange"
-  public displayName = false
-  public expression = false
-
-  public titleStyles = {
-    fontStyle: "italic",
-    color: "blue",
-  }
-  public greeting = ""
+  public color = "green"
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  greetUser(){
-    return "Hello " + this.name
-  }
-
-  onClick(event: any){
-    console.log("Welcome")
-    console.log(event)
-    this.greeting = event.type
-  }
-
-  logMessage(value: any) {
-    console.log(value)
   }
 
 }
