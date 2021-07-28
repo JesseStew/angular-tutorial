4b094e3cb12801cf06befad8afd8891ed4feaa9c
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-07-28 17:44:32 -0500

    Routing and Navigation Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeService } from './employee.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DepartmentListComponent } from './department-list/department-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    EmployeeListComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    routingComponents,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
